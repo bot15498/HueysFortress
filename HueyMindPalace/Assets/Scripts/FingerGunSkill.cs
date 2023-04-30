cccf297fba699570beadc768d485ae2c21196442
skyler <konata.izumi20@gmail.com>
2023-04-30 13:50:00 -0400

    fix skill cooldown

--- HueyMindPalace/Assets/Scripts/FingerGunSkill.cs
+++ HueyMindPalace/Assets/Scripts/FingerGunSkill.cs
@@ -62,8 +62,8 @@ public class FingerGunSkill : MonoBehaviour
                 if (store != null && store.hasReduceCooldown)
                 {
                     // Tick down cooldown twice. 
-                    skillInfo.maxCooldown = Mathf.Floor(skillInfo.maxCooldown/ 2);
-                    skillInfo.currentCooldown = Mathf.Floor(skillInfo.currentCooldown / 2);
+                    skillInfo.maxCooldown -= 1;
+                    skillInfo.currentCooldown -= 1;
                 }
 
                 isAiming = false;
